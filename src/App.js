132b932fd0e673d89d15fbdeba6aa88304d97c4c
Jaydeep Vachhani <jrv238@gmail.com>
2021-02-27 17:08:27 +0530

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import DashBoard from "./pages/Dashboard";
 import Detail from "./pages/Detail";
 function App() {
   return (
-    <Router exact basename="/intelpixel">
+    <Router>
       <Switch>
         <Route exact path="/" component={DashBoard}></Route>
         <Route exact path="/detail" component={Detail}></Route>
